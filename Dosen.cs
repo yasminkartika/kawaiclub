df22ef36c861fb87ae7f800ef4beddf96e251b95
yasminkartika <yasminkartika.idr@gmail.com>
2023-05-29 20:32:35 +0700

    inisialisasi project

---
+++ Dosen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemro
+{
+    internal class Dosen
+    {
+      
+        private string Nama { get; set; }
+        private int Nik { get; set; }
+        private string Gender { get; set; }
+        private string Course { get; set; }
+
+        public Dosen()
+        {
+            
+            Nama = "Dosen Pengampu";
+            Nik = 123456789;
+            Gender = "Male";
+            Course = "Pemrograman Berorientasi Objek";
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Nama: " + Nama);
+            Console.WriteLine("NIK: " + Nik);
+            Console.WriteLine("Gender: " + Gender);
+            Console.WriteLine("Course: " + Course);
+        }
+
+        public void attDosen(string nama, int nik)
+        {
+            Nama = nama;
+            Nik = nik;
+        }
+
+        public void addCourse(string newCourse)
+        {
+            Course += ", " + newCourse;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            Console.WriteLine("Default Dosen Pengampu:");
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru:");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan Mata Kuliah Baru:");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
